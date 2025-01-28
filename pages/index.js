dd1cb547a822b823fcdb167c3ebd1a47f8feb6fe
Nijan Adhikari <nijanadhikari@Nijans-MacBook-Air.local>
2025-01-28 12:19:51 -0800

    changes to path

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import { Poppins } from "next/font/google";
-import Home from "../components/layout/home";
-import About from "../components/layout/about";
-import { Navbar } from "../components/global/navbar";
-import Services from "../components/layout/services";
-import Portfolio from "../components/layout/portfolio";
-import Footer from "../components/global/footer";
-import Loader from "../components/global/loader";
+import Home from "@/components/layout/home";
+import About from "@/components/layout/about";
+import { Navbar } from "@/components/global/navbar";
+import Services from "@/components/layout/services";
+import Portfolio from "@/components/layout/portfolio";
+import Footer from "@/components/global/footer";
+import Loader from "@/components/global/loader";
 import { useEffect, useState } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import Contact from "../components/layout/contact";
-import { Startups } from "../components/layout/startups";
+import Contact from "@/components/layout/contact";
+import { Startups } from "@/components/layout/startups";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
